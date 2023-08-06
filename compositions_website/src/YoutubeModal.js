6b5652533f410d8c827000cb34dbf43ef38e4730
Steve Ryan <syntithenai@gmail.com>
2023-08-07 01:00:24 +1000

    auto add and build

--- compositions_website/src/YoutubeModal.js
+++ compositions_website/src/YoutubeModal.js
@@ -7,18 +7,18 @@ import {useEffect} from 'react'
 import YouTube from 'react-youtube';
 
 export default function YoutubeModal({youtubeId, meta}) {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const opts = {
-      height: '600',
-      width: '90%',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
-    };
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const opts = {
+	  height: '600',
+	  width: '90%',
+	  playerVars: {
+		// https://developers.google.com/youtube/player_parameters
+		autoplay: 1,
+	  },
+	};
 
 var m = meta ? meta  : {}
   function _onReady(event) {
@@ -38,9 +38,7 @@ var m = meta ? meta  : {}
 		  <Modal dialogClassName="media-modal" show={show} onHide={handleClose} >
 		  <Modal.Header closeButton ><h3>{m.title}</h3> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>{m.composer}</i></Modal.Header>
 		  	<YouTube videoId={youtubeId} opts={opts} onReady={_onReady} />
-		
 		  </Modal>
-
 	   </>}
       
     </>
